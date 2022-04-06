6992dcfabaac21c809fa993bc4038e835695bf52
Snipermods <andrew.kidd99@gmail.com>
2022-04-06 17:58:26 +0100

    removed color in template js didn't work

--- template.js
+++ template.js
@@ -15,9 +15,7 @@ export default ({markup, css}) => {
                 text-decoration: none;
                 color: #061d95
               }
-              h6{
-                color: #ffffff
-              }
+             
           </style>
         </head>
         <body>
