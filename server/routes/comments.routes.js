1a78112786726d013b2ae97371a3ff072c3f985c
Snipermods <andrew.kidd99@gmail.com>
2022-04-10 22:51:27 +0100

    working comment.routes

---
+++ server/routes/comments.routes.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import commentsCtrl from '../controllers/comments.controller'
+import authCtrl from '../controllers/auth.controller'
+
+const router = express.Router()
+
+router.route('/api/comments')
+  .get(authCtrl.requireSignin,  commentsCtrl.list)
+  .post(authCtrl.requireSignin, commentsCtrl.create)
+
+
+
+
+
+
+
+export default router
