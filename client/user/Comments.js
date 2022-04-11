9c4d05741d182e813a14d030d504a24122d0bf5a
Snipermods <andrew.kidd99@gmail.com>
2022-04-11 01:39:19 +0100

    Styling change on comment pahe

--- client/user/Comments.js
+++ client/user/Comments.js
@@ -14,16 +14,50 @@ import Person from '@material-ui/icons/Person'
 import {Link} from 'react-router-dom'
 import {list} from './api-user.js'
 
+
+import React, {useState} from 'react'
+import Card from '@material-ui/core/Card'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField'
+import Typography from '@material-ui/core/Typography'
+import Icon from '@material-ui/core/Icon'
+import { makeStyles } from '@material-ui/core/styles'
+import {create} from './api-user.js'
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogContentText from '@material-ui/core/DialogContentText'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import {Link} from 'react-router-dom'
+
+
 const useStyles = makeStyles(theme => ({
-  root: theme.mixins.gutters({
-    padding: theme.spacing(1),
-    margin: theme.spacing(5)
-  }),
-  title: {
-    margin: `${theme.spacing(4)}px 0 ${theme.spacing(2)}px`,
-    color: theme.palette.openTitle
-  }
-}))
+    card: {
+      maxWidth: 600,
+      margin: 'auto',
+      textAlign: 'center',
+      marginTop: theme.spacing(5),
+      paddingBottom: theme.spacing(2)
+    },
+    error: {
+      verticalAlign: 'middle'
+    },
+    title: {
+      marginTop: theme.spacing(2),
+      color: theme.palette.openTitle
+    },
+    textField: {
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+      width: 300
+    },
+    submit: {
+      margin: 'auto',
+      marginBottom: theme.spacing(2)
+    }
+  }))
 
 export default function Users() {
   const classes = useStyles()
@@ -62,9 +96,18 @@ export default function Users() {
                       
                     </ListItem>
                  </Link>
+                 
                })
              }
         </List>
+        
       </Paper>
+
+      
+        
+      
     )
+
+    
+    
 }
