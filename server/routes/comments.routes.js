4b7847b1856186ca304d9d9ee6c8fd6d2c1bcb32
Snipermods <andrew.kidd99@gmail.com>
2022-04-11 17:14:02 +0100

    Changed import

--- server/routes/comments.routes.js
+++ server/routes/comments.routes.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import commentsCtrl from '../controllers/comments.controller'
 import authCtrl from '../controllers/auth.controller'
-import userCtrl from '../controllers/auth.controller'
+import userCtrl from '../controllers/user.controller'
 
 const router = express.Router()
 
@@ -18,6 +18,6 @@ router.route('/api/commentid/:userid/:commentid')
 
 
 router.param('userid', userCtrl.userByID)
-router.param('commentid', commentsCtrl.commentByID)//comment
+router.param('commentid', commentsCtrl.commentByID)
 
 export default router
