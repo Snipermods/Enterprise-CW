2b0b88a8e91b797329be74007dfe7f27168da1f3
Snipermods <andrew.kidd99@gmail.com>
2022-04-11 18:45:02 +0100

    added a delete button to comments.js

--- client/user/Comments.js
+++ client/user/Comments.js
@@ -145,6 +145,7 @@ export default function createComments() {
                       <button size = "small" >Reply!</button>
                       
                     </ListItem>
+                    <DeleteUser userId={user._id}/>
 
   </div>  )})     
           
